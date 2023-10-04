21620510e278df54a430c7a7ae669a97ba3da00b
Explorer4545 <146115999+Explorer4545@users.noreply.github.com>
2023-10-04 23:18:40 +0530

    Delete Basics directory

--- Basics/variables&datatypes.js
+++
@@ -1,9 +0,0 @@
-const accountId=12345
-let accountname="aman123"
-var city="Kanpur"
-var accountstate;
-
-//try not to use var and instead use let and const as they are block scoped
-
-console.table([accountId,accountname,city,accountstate]);
-
