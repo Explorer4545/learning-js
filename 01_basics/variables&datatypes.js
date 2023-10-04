b137d8b73594bb75d0c7dcf9a8e788819d9eaeb9
Explorer4545 <146115999+Explorer4545@users.noreply.github.com>
2023-10-04 17:47:28 +0000

    updated title name

---
+++ 01_basics/variables&datatypes.js
@@ -0,0 +1,9 @@
+const accountId=12345
+let accountname="aman123"
+var city="Kanpur"
+var accountstate;
+
+//try not to use var and instead use let and const as they are block scoped
+
+console.table([accountId,accountname,city,accountstate]);
+
